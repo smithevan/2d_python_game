a53bfdbdc947f4241871723f6b6b24972adb6ca1
Evan Smith <smith.m.evan@gmail.com>
2020-08-05 19:57:52 +0100

    code was fine, launch error caused by old version of pygame, pip installed it

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import pygame
-import os
-import time
-import random 
+# import os
+#import time
+
+# import random
 
 pygame.init()
 
-print ("hello")
+screen = pygame.display.set_mode([700, 700])
+pygame.display.set_caption("Name")
 
+
+def main():
+    running = True
+    refresh_rate = 60
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(refresh_rate)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+
+    pygame.quit()
+
+
+main()
