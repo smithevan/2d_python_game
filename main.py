e83ee47f9fca25c69f2817d33d32efeac1b99463
Evan Smith <smith.m.evan@gmail.com>
2020-08-05 23:34:03 +0100

    snake is movable

--- main.py
+++ main.py
@@ -12,12 +12,20 @@ BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "te
 
 SNAKE = pygame.transform.scale(pygame.image.load(os.path.join("assets", "solid_black.png")), (10, 10))
 
+'''class Snake(self, x, y):
+    
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y '''
+   
+
+
 def main():
     running = True
     refresh_rate = 60
     clock = pygame.time.Clock()
-    snake_x = randint(10, 600)
-    snake_y = randint(10, 600)
+    snake_x = randint(100, 600)
+    snake_y = randint(100, 600)
 
     def redraw():
         SCREEN.blit(BACKGROUND, (0,0))
@@ -32,6 +40,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            snake_x -= 2
+        if keys[pygame.K_RIGHT]:
+            snake_x += 2
+        if keys[pygame.K_UP]:
+            snake_y -= 2
+        if keys[pygame.K_DOWN]:
+            snake_y += 2
 
        
 
